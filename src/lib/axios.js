a162118e903a7327b4f95c27941c41f00f17861c
daksh <dakshhardiya34@gmail.com>
2025-08-25 07:28:20 +0530

    Project completed

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 
-export const axiosInstance = new axios.create({
-    baseURL:import.meta.env.MODE ==="development"? 'http://localhost:5000/api':"https://fashionate-backend.up.railway.app/api",
-    withCredentials:true,
-})
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://fashionate-backend.up.railway.app/api",
+  withCredentials: true,
+});
